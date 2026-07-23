bc1d504f7bc9b15794a4b567b5cbdb50e6e3e8a6
Paulo Gabriel <167820551+paulogabrielvieira@users.noreply.github.com>
2026-07-23 11:44:01 +0000

    backtest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from functions.plot import plot_history
+from functions.backtest import ma_strategy
 
 st.title('Stocks History')
 st.write('Look the stock values.')
@@ -9,5 +10,29 @@ ticker = st.sidebar.text_input(
     value = 'NVDA'
 )
 
+ma_short_window = st.sidebar.number_input(
+    "MA Short",
+    min_value = 1,
+    max_value = 100,
+    value = 9,
+    step = 1
+)
+
+ma_long_window = st.sidebar.number_input(
+    "MA Long",
+    min_value = 1,
+    max_value = 300,
+    value = 72,
+    step = 1
+)
+
 fig = plot_history(ticker)
 st.plotly_chart(fig)
+
+df = ma_strategy(
+    ticker = ticker,
+    ma_short = ma_short_window,
+    ma_long = ma_long_window
+)
+
+st.dataframe(df)
